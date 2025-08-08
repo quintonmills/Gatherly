8da8b85a83be8e477ab5c63487f5c2baa7d124b5
quintonmills <quintonmills98@gmail.com>
2025-08-08 11:13:18 -0400

    push project to github

---
+++ frontend/src/pages/EditEventPage.js
@@ -0,0 +1,8 @@
+import EventForm from "../components/EventForm";
+import { useRouteLoaderData } from "react-router-dom";
+
+function EditEventsPage() {
+  const data = useRouteLoaderData("event-detail");
+  return <EventForm event={data.event} />;
+}
+export default EditEventsPage;
---
+++ frontend/src/pages/EventsPage.js
@@ -0,0 +1,31 @@
+import { useLoaderData } from "react-router-dom";
+import EventsList from "../components/EventsList";
+
+function EventsPage() {
+  const data = useLoaderData();
+  if (data.isError) {
+    return <p>{data.message}</p>;
+  }
+  const events = data.events;
+  return (
+    <>
+      <EventsList events={events} />
+    </>
+  );
+}
+
+export default EventsPage;
+
+export async function loader() {
+  const response = await fetch("http://localhost:8080/events");
+
+  if (!response.ok) {
+    //return { isError: true, message: " could not fetch events" };
+    // throw new Response(JSON.stringify({ message: "could not fetch events" }), {
+    //   status: 500,
+    // });
+    return { message: "could not fetch events" }, { status: 500 };
+  } else {
+    return response;
+  }
+}
